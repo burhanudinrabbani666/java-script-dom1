699967d8ca7d31847ac3dafa52b4cc327403f78a
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2025-12-29 14:34:21 +0700

    feat: manipulating value

--- guess-my-number/script.js
+++ guess-my-number/script.js
@@ -1,8 +1,6 @@
-## Selecting and manipulating
+"use strict";
 
-We can set the HTML value from JavaScript by taking the element first and then changing the value.
-
-```js
+// Set content inside html
 document.querySelector(".message").textContent = "Correct Number! 🥳";
 
 document.querySelector(".number").textContent = "69";
@@ -11,6 +9,3 @@ document.querySelector(".score").textContent = "66";
 // Get Value
 document.querySelector(".guess").value = 123;
 console.log(document.querySelector(".guess").value);
-```
-
-[Next: Handling Click Event](./03-handling-click-event.md)
--- script.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-
-const getElement = document.querySelector(".message");
